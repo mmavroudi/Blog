8a6a620924acf52ed951d00d09fb88a5bcd8968c
mmavroudi <mmavroudi@gmail.com>
2022-10-10 15:11:18 +0300

    Creating a model for Blog Post View #13

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    slug = models.SlugField(blank=True, unique=True)
