80e214ae36cee12cc89a488f70de5b06b68e849c
mmavroudi <mmavroudi@gmail.com>
2022-10-11 11:19:03 +0300

    Correcting URL for task 2 #13

--- mysite/urls.py
+++ mysite/urls.py
@@ -25,7 +25,7 @@ import projects.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', projects.views.home, name='home'),
-    path('post_detail', projects.views.post_detail_view, name='post_detail_view'),
+    path('post_detail_view', projects.views.post_detail_view, name='posts'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
