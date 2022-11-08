4b84b22d27841edd194ee998eddb72eefa4b4aa7
cat <kkampardi@gmail.com>
2022-11-08 12:20:43 +0200

    remove multiple return for __str__ function each function must have one return

--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     price = models.IntegerField()
@@ -12,4 +13,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-        return self.content
