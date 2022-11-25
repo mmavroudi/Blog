432183b5dea102a5533ea1fa785e2b341e6710cb
mmavroudi <mmavroudi@gmail.com>
2022-11-25 12:54:33 +0200

    Relate Posts with Categories with many-to-many relationships #30

--- projects/models.py
+++ projects/models.py
@@ -3,25 +3,33 @@ from django.db import models
 
 # Create your models here.
 
-
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    price = models.IntegerField()
-    content = models.TextField()
-    image = models.ImageField(upload_to='uploads', height_field=None, width_field=None, max_length=100)
-    slug = models.SlugField(blank=True, unique=True)
-
-    def __str__(self):
-        return self.title
-
 class Category(models.Model):
     title = models.CharField(max_length=50)
     subtitle = models.CharField(max_length=100)
     slug = models.SlugField(blank=True, unique=True)
     thumbnail = models.ImageField(blank=True, unique=True)
 
+    class Meta:
+        ordering = ['title']
+
     def __str__(self):
         return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    categories = models.ManyToManyField(Category)
+    price = models.IntegerField()
+    content = models.TextField()
+    image = models.ImageField(upload_to='uploads', height_field=None, width_field=None, max_length=100)
+    slug = models.SlugField(blank=True, unique=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
 
 
 
